8095afdcf5e2030407ad8756f03c9093041820c3
Kushagra <kushagra100chhabra@gmail.com>
2025-10-31 19:19:53 +0530

    HVC

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
     {
       id: "github",
       element: <Github className="w-5 h-5 sm:w-6 sm:h-6" />,
-      href: "https://github.com",
+      href: "https://github.com/Kushagra-369",
       hoverColor: "hover:text-blue-400",
       external: true,
     },
