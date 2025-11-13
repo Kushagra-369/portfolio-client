f015f73d5a0acaf2cdfc71a94477e10a94a02e23
Kushagra <kushagra100chhabra@gmail.com>
2025-11-13 17:32:22 +0530

    GFCD

--- src/Components/Projects/Project.tsx
+++ src/Components/Projects/Project.tsx
@@ -13,7 +13,7 @@ export default function Project() {
 
     const fetchProjects = async () => {
       try {
-        const res = await fetch("http://localhost:1080/get_all_project");
+        const res = await fetch("http://https://portfolio-server-u68u.onrender.com/get_all_project");
         const data = await res.json();
         setProjects(data.data);
       } catch (error) {
