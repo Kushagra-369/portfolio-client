60cf2021e8233b6dae593443d530a7a6f09c9b74
Kushagra <knowledge4040god@gmail.com>
2026-06-06 04:30:21 +0530

    HG

--- src/Components/AI/api/server.py
+++ src/Components/AI/api/server.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://portfolio-client-n5v7.vercel.app/"
+        "https://portfolio-client-n5v7.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
